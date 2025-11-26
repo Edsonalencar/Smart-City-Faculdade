edf164e9dbb351daf1978ef7f8ecae1d52de9677
Edson Alencar <edson.cesar.alencar@gmail.com>
2025-11-25 22:24:58 -0300

    Start project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import utils.KeyManager;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Gera e salva chaves RSA em arquivos
+        KeyManager.generateAndSaveAllKeys();
+    }
+}
